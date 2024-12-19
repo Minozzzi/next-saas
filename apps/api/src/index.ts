5b43f3317a40437daf917cf4d4e8c5e448320763
Guilherme Minozzi de Sa <minozzi60@gmail.com>
2024-12-19 08:25:44 -0300

    feat: add setup fastify and database models

--- apps/api/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { defineAbilitiesFor } from '@saas/auth'
-
-const ability = defineAbilitiesFor({ role: 'ADMIN' })
-
-const userCanInviteSomeoneElse = ability.can('invite', 'User')
-
-console.log('userCanInviteSomeoneElse: ', userCanInviteSomeoneElse)
