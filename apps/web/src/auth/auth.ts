0dc31afdb4f717739add556a48197e456631393e
Guilherme Minozzi de Sa <minozzi60@gmail.com>
2025-03-21 16:28:59 -0300

    feat: add auth function to validate if user is logged and valid token

--- apps/web/src/auth/auth.ts
+++ apps/web/src/auth/auth.ts
@@ -1,6 +1,27 @@
 import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+
+import { getProfile } from '@/http/get-profile'
 
 export async function isAuthenticated() {
   const cookiesStore = await cookies()
   return !!cookiesStore.get('token')?.value
+}
+
+export async function auth() {
+  const cookieStore = await cookies()
+
+  const token = cookieStore.get('token')?.value
+
+  if (!token) {
+    redirect('/auth/sign-in')
+  }
+
+  const { user } = await getProfile()
+
+  if (user) {
+    return { user }
+  }
+
+  redirect('/api/auth/sign-out')
 }
