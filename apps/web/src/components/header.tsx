d2ae191ad85b0326cd5b5c13503d304d6a6f1fee
Guilherme Minozzi <minozzi60@gmail.com>
2025-04-14 20:03:07 -0300

    feat: show user projects if user be able to see

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,9 +1,13 @@
 import { Flower2Icon, SlashIcon } from 'lucide-react'
 
+import { getAbility } from '@/auth/auth'
+
 import { OrganizationSwitcher } from './organization-switcher'
 import { ProfileButton } from './profile-button'
 
-export function Header() {
+export async function Header() {
+  const permissions = await getAbility()
+
   return (
     <header className="mx-auto flex max-w-[1200px] items-center justify-between">
       <div className="flex items-center gap-3">
@@ -12,6 +16,8 @@ export function Header() {
         <SlashIcon className="text-border size-3 -rotate-[24deg] dark:invert" />
 
         <OrganizationSwitcher />
+
+        {permissions?.can('get', 'Project') && <p>Projects</p>}
       </div>
 
       <div className="flex items-center gap-4">
