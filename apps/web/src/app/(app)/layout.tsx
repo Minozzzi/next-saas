ab810c8fdaf423cf94404f49e5233d96426c79a0
Guilherme Minozzi <minozzi60@gmail.com>
2025-04-22 22:04:12 -0300

    refactor: add header on layout.tsx

--- apps/web/src/app/(app)/layout.tsx
+++ apps/web/src/app/(app)/layout.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation'
-import { Fragment } from 'react'
 
 import { isAuthenticated } from '@/auth/auth'
+import { Header } from '@/components/header'
 
 export default async function AppLayout({
   children,
@@ -12,5 +12,11 @@ export default async function AppLayout({
     redirect('/auth/sign-in')
   }
 
-  return <Fragment>{children}</Fragment>
+  return (
+    <div className="space-y-4 p-4">
+      <Header />
+
+      <main className="mx-auto w-full max-w-[1200px]">{children}</main>
+    </div>
+  )
 }
