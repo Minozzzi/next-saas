0231c5a48bbbb0a2dcf78ba4a0227c6798e362a2
Guilherme Minozzi <minozzi60@gmail.com>
2025-04-23 23:02:03 -0300

    feat: add variant success on alert component

--- apps/web/src/components/ui/alert.tsx
+++ apps/web/src/components/ui/alert.tsx
@@ -11,6 +11,8 @@ const alertVariants = cva(
         default: 'bg-background text-foreground',
         destructive:
           'text-destructive-foreground [&>svg]:text-current *:data-[slot=alert-description]:text-destructive-foreground/80 border-destructive dark:text-red-400 dark:[&>svg]:text-red-400 dark:*:data-[slot=alert-description]:text-red-400/80 dark:border-red-400',
+        success:
+          'text-green-500 [&>svg]:text-current *:data-[slot=alert-description]:text-green-500/80 border-green-500 dark:text-green-400 dark:[&>svg]:text-green-400 dark:*:data-[slot=alert-description]:text-green-400/80 dark:border-green-400',
       },
     },
     defaultVariants: {
