9274641c19228beb73aabb66f5a78f23f112f262
Guilherme Minozzi <minozzi60@gmail.com>
2025-05-15 22:12:38 -0300

    refactor: move organization form folder

--- apps/web/src/app/(app)/create-organization/page.tsx
+++ apps/web/src/app/(app)/create-organization/page.tsx
@@ -1,6 +1,6 @@
 import { Header } from '@/components/header'
 
-import { OrganizationForm } from './organization-form'
+import { OrganizationForm } from '../organization/organization-form'
 
 export default function CreateOrganizationPage() {
   return (
