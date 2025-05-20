9317f8ed52d18c1eb8677ded9adf6245e42a66ed
Guilherme Minozzi <minozzi60@gmail.com>
2025-05-20 19:54:48 -0300

    feat: add get members http request

---
+++ apps/web/src/http/get-members.ts
@@ -0,0 +1,24 @@
+import type { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+type GetMembersResponse = {
+  members: {
+    id: string
+    userId: string
+    role: Role
+    name: string | null
+    email: string
+    avatarUrl: string | null
+  }[]
+}
+
+export async function getMembers(
+  organization: string,
+): Promise<GetMembersResponse> {
+  const result = await api
+    .get(`organizations/${organization}/members`)
+    .json<GetMembersResponse>()
+
+  return result
+}
