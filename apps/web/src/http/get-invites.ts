9785436c466f1dd432e11a76118fc0f9ff75d649
Guilherme Minozzi <minozzi60@gmail.com>
2025-05-22 21:47:45 -0300

    feat: add http request to get invites

---
+++ apps/web/src/http/get-invites.ts
@@ -0,0 +1,28 @@
+import type { Role } from '@saas/auth'
+
+import { api } from './api-client'
+
+type GetInvitesResponse = {
+  invites: {
+    id: string
+    role: Role
+    email: string
+    createdAt: string
+    author: {
+      id: string
+      name: string | null
+    } | null
+  }[]
+}
+
+export async function getInvites(org: string): Promise<GetInvitesResponse> {
+  const result = await api
+    .get(`organizations/${org}/invites`, {
+      next: {
+        tags: [`${org}/invites`],
+      },
+    })
+    .json<GetInvitesResponse>()
+
+  return result
+}
